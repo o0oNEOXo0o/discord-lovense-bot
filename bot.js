9379f26621cd02236029ac43f498fa5368d6631a
o0oNEOXo0o <arne.werner87@gmail.com>
2018-09-25 00:45:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,40 +4,43 @@ var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+	token: auth.token,
+	autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'play':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Lets play'
-                });
-	    case 'patreon':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Support us on Patreon: https://www.patreon.com/lovense'
-                });
-            break;
+	var args = message.substring(1).split(' ');
+	var cmd = args[0];
+	    
+	args = args.splice(1);
+	switch(cmd) {
+		case 'play':
+			bot.sendMessage({
+			to: channelID,
+			message: 'Lets play',
+			message.react("ðŸ‘")
+			.then(message.react("ðŸ‘Ž"))
+			.catch(console.error);
+		});
+		case 'patreon':
+			bot.sendMessage({
+			to: channelID,
+			message: 'Support us on Patreon: https://www.patreon.com/lovense'
+		});
+		break;
             // Just add any case commands if you want to..
-         }
+	}
      }
 });
