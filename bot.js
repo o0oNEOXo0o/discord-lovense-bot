2dc0c612861ee115dbdb1868123179378be9d81b
o0oNEOXo0o <arne.werner87@gmail.com>
2018-09-25 20:24:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
